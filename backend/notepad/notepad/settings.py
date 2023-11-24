14a89c347ddfea65177dbdd58f6e51eb59dc4773
IbrahimMalgwi <ibrahimganamalgwi@outlook.com>
2023-11-25 00:25:07 +0100

    update

--- backend/notepad/notepad/settings.py
+++ backend/notepad/notepad/settings.py
@@ -123,3 +123,22 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'discord': {
+            'level': 'INFO',
+            'class': 'discord_webhook.WebhookHandler',
+            'url': 'YOUR_DISCORD_WEBHOOK_URL',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['discord'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+    },
+}
--- backend/notepad/notes/views.py
+++ backend/notepad/notes/views.py
@@ -1,6 +1,12 @@
 from rest_framework import viewsets
 from .models import Note
 from .serializers import NoteSerializer
+import logging
+logger = logging.getLogger(__name__)
+
+def create_note(request):
+    # Your create note logic
+    logger.info("Note created: {}".format(note_id))
 
 class NoteViewSet(viewsets.ModelViewSet):
     queryset = Note.objects.all()
